d24e82c8e762dc0e04a5f5adcd41ef6ac5071518
Isai Rodriguez <macover1507@hotmail.com>
2022-08-22 13:31:59 -0500

    vista para escoger sucursales implementada v2

--- app/src/main/java/com/spvacfarmacia/Sucursales.kt
+++ app/src/main/java/com/spvacfarmacia/Sucursales.kt
@@ -11,7 +11,6 @@ import com.spvacfarmacia.databinding.ActivitySucursalesBinding
 
 class Sucursales : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivitySucursalesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,21 +19,7 @@ class Sucursales : AppCompatActivity() {
         binding = ActivitySucursalesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_sucursales)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_sucursales)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
